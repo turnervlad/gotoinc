e32282148970eafa3f3bc2b75cef4113e8abe7b6
vladvlad93 <33132561+vladvlad93@users.noreply.github.com>
2020-11-16 19:54:11 +0200

    add show/hide logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useCallback } from "react";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./App.css";
@@ -10,81 +10,94 @@ function App() {
   const [gValue, setGValue] = useState(0);
   const [bValue, setBValue] = useState(0);
   const [color, setColor] = useState(`rgb(${rValue}, ${gValue}, ${bValue})`);
+  const [isSliderHidden, setSlidersVisibility] = useState(false);
 
   const prevState = useRef({ r: rValue, g: gValue, b: bValue });
 
-  const submitOnClick = () => {
+  const onSubmit = useCallback(() => {
     prevState.current = { r: rValue, g: gValue, b: bValue };
-  };
+  }, [rValue, gValue, bValue]);
+
+  const onSlidersBlockToggle = useCallback(() => {
+    setSlidersVisibility(!isSliderHidden);
+  }, [isSliderHidden]);
 
   useEffect(() => {
     setColor(`rgb(${rValue}, ${gValue}, ${bValue})`);
-  }, [rValue, gValue, bValue]); 
+  }, [rValue, gValue, bValue]);
 
-  const cancelOnClick = () => {
+  const onCancel = useCallback(() => {
     setRValue(prevState.current.r);
     setGValue(prevState.current.g);
     setBValue(prevState.current.b);
-  };
+  }, []);
+
+  const onChangeR = useCallback((v) => {
+    setRValue(v);
+  }, []);
+
+  const onChangeG = useCallback((v) => {
+    setGValue(v);
+  }, []);
+
+  const onChangeB = useCallback((v) => {
+    setBValue(v);
+  }, []);
 
   return (
     <div className="app-wrapper">
       <Square color={color} />
 
-      <div>
+      <Button
+        text={isSliderHidden ? "Show slider" : "Hide slider"}
+        onClick={onSlidersBlockToggle}
+      />
+
+      {!isSliderHidden ? (
         <div>
-          <Slider
-            min={0}
-            max={255}
-            value={rValue}
-            onChange={(v) => {
-              setRValue(v);
-            }}
-            railStyle = {{
-              backgroundColor: "red",
-            }}
-          />
-          <text>{rValue}</text>
+          <div>
+            <Slider
+              min={0}
+              max={255}
+              value={rValue}
+              onChange={onChangeR}
+              railStyle={{
+                backgroundColor: "red",
+              }}
+            />
+            <text>{rValue}</text>
+          </div>
+
+          <div>
+            <Slider
+              min={0}
+              max={255}
+              value={gValue}
+              onChange={onChangeG}
+              railStyle={{
+                backgroundColor: "green",
+              }}
+            />
+            <text>{gValue}</text>
+          </div>
+
+          <div>
+            <Slider
+              min={0}
+              max={255}
+              value={bValue}
+              onChange={onChangeB}
+              railStyle={{
+                backgroundColor: "blue",
+              }}
+            />
+            <text>{bValue}</text>
+          </div>
+
+          <Button text={"Submit"} onClick={onSubmit} />
+          <Button text={"Cancel"} onClick={onCancel} />
         </div>
-
-
-        <div>
-          <Slider
-            min={0}
-            max={255}
-            value={gValue}
-            onChange={(v) => {
-              setGValue(v);
-            }}
-            railStyle = {{
-              backgroundColor: "green"
-            }}
-          />
-          <text>{gValue}</text>
-        </div>
-
-        <div>
-          <Slider
-            min={0}
-            max={255}
-            value={bValue}
-            onChange={(v) => {
-              setBValue(v);
-            }}
-            railStyle = {{
-              backgroundColor: "blue"
-            }}
-          />
-          <text>{bValue}</text>
-        </div>
-      </div>
-
-      <Button 
-        text={"Submit"}
-        onClick={submitOnClick} 
-        
-        />
-      <Button text={"Cancel"} onClick={cancelOnClick} />
+      ) : null}
     </div>
   );
 }
